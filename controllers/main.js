03df9ef70d78bb759b336d43c598b3d8eedf0993
Jose Barranco <jdcoding01@gmail.com>
2020-02-10 12:25:49 -0400

    @

--- controllers/main.js
+++ controllers/main.js
@@ -3,4 +3,4 @@ const path = require("path");
 exports.root = async (req, reply) => { reply.render("index") };
 exports.web = async (req, reply) => { reply.render("web") };
 exports.mobile = async (req, reply) => { reply.render("mobile") };
-exports.image = async (req, reply) => { reply.sendFile(path.resolve('./controllers/image.jpg')) };
+exports.image = async (req, reply) => { reply.sendFile(path.resolve('./app/controllers/image.jpg')) };
